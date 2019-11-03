12bd6f1ab0d8769f2288a551028e8917818e5cad
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-11-03 12:57:56 +0900

    implement abstract product

---
+++ src/abstractfactorypattern/Page.kt
@@ -0,0 +1,33 @@
+package abstractfactorypattern
+
+import java.io.FileWriter
+import java.io.IOException
+
+/**
+ * Abstract product class.
+ */
+abstract class Page(
+    protected val title: String,
+    protected val author: String
+) {
+    protected val content = mutableListOf<Item>()
+
+    fun add(item: Item) {
+        content.add(item)
+    }
+
+    fun output() {
+        try {
+            val filename = "$title.html"
+            FileWriter(filename).apply {
+                write(makeHTML())
+                close()
+            }
+            println("$filename を作成しました")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    abstract fun makeHTML(): String
+}
