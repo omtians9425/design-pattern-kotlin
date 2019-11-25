25cf766db5e65f4c2359a9e2a6947d176ef2f67e
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-11-25 22:28:23 +0900

    implement main

--- src/bridgepattern/Display.kt
+++ src/bridgepattern/Display.kt
@@ -2,7 +2,7 @@ package bridgepattern
 
 /**
  * The top hierarchy of the feature.
- * It's methods delegate to [DisplayImpl].
+ * It's methods delegate to [DisplayImpl] that is bridge between feature and implementation.
  */
 open class Display(private val impl: DisplayImpl) {
 
